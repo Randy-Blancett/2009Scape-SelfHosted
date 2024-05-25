d1194d83fa46815a91f8be0a6e304d1f242ac310
Randy Blancett <Randy.Blancett@gmail.com>
2024-05-24 22:58:23 -0400

    Ticket #1
    Add Default DAta

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,14 @@ FROM eclipse-temurin:11-jre-alpine
 # Fake it til you make it - let's go home
 WORKDIR /app
 ENV WORLD_CONFIG_FILE=default.conf
+ENV DEFAULT_DATA_DIR=/app/default_data
 
 RUN mkdir -p /app/worldprops
+RUN mkdir -p ${DEFAULT_DATA_DIR}
 COPY ./server.jar /app
 COPY ./default.conf /app
 COPY ./start.sh /app
+COPY ./default_data/* ${DEFAULT_DATA_DIR}
 RUN chmod a+x /app/start.sh
 
 # Run it
--- resources/start.sh
+++ resources/start.sh
@@ -3,12 +3,50 @@
 ls -l -h
 pwd
 mkdir -p /app/worldprops
+mkdir -p /app/data
+
+
+DIR_BOT_DATA=/app/data/botdata
+DIR_CACHE=/app/data/cache
+DIR_CONFIGS=/app/data/configs
+DIR_ECO=/app/data/eco
+FILE_OBJECT_PARSER=/app/data/ObjectParser.xml
 
 WORLD_PROP_FILE=worldprops/${WORLD_CONFIG_FILE}
 if [ ! -f /app/${WORLD_PROP_FILE} ]
 then
     echo "/app/${WORLD_PROP_FILE} does not exist copying the default config."
     cp /app/default.conf /app/${WORLD_PROP_FILE} 
+fi
+
+if [ ! -f ${FILE_OBJECT_PARSER} ]
+then
+    echo "${FILE_OBJECT_PARSER} does not exist copying the default data."
+    cp ${DEFAULT_DATA_DIR}/ObjectParser.xml ${FILE_OBJECT_PARSER}
+fi
+
+if [ ! -d ${DIR_BOT_DATA} ]
+then
+    echo "${DIR_BOT_DATA} does not exist copying the default data."
+    cp ${DEFAULT_DATA_DIR}/botdata ${DIR_BOT_DATA}
+fi
+
+if [ ! -d ${DIR_CACHE} ]
+then
+    echo "${DIR_CACHE} does not exist copying the default data."
+    cp ${DEFAULT_DATA_DIR}/cache ${DIR_CACHE}
+fi
+
+if [ ! -d ${DIR_CONFIGS} ]
+then
+    echo "${DIR_CONFIGS} does not exist copying the default data."
+    cp ${DEFAULT_DATA_DIR}/configs ${DIR_CONFIGS}
+fi
+
+if [ ! -d ${DIR_ECO} ]
+then
+    echo "${DIR_ECO} does not exist copying the default data."
+    cp ${DEFAULT_DATA_DIR}/eco ${DIR_ECO}
 fi
 
 ls -l -h /app/worldprops
